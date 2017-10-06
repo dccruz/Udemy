f152e09a15207cbbafe0b9f63eb9c0197c8136f0
dccruz <dccoder@icloud.com>
2017-10-05 20:21:05 -0700

    Working With The JavaScript Console

---
+++ JavaScript For Beginners/Section 1 /3_Working With The JavaScript Console/js.js
@@ -0,0 +1,7 @@
+
+// document.write("This is our javascript being written first before anything else);
+// SyntaxError: unterminated string literal
+// so we  have to fix the syntax issue with the missing quotation
+
+document.write("This is our javascript being written first before anything else");
+Console.log("This is text that will be sent directly to the console to let us know that our program completed successfully");
