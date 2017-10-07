f7bc9a1b15bc40262f276a3e25048e828e3b6181
dccruz <dccoder@icloud.com>
2017-10-06 20:29:22 -0700

    JavaScript Console Challenge

---
+++ JavaScript For Beginners/Section 1 /4_JavaScript Console Challenge/js.js
@@ -0,0 +1,15 @@
+
+
+
+// Original Code: 
+// alert("Let's Begin The Challenge!");
+// document.write("Congrats You Fixed This Line Of Code!";
+// Console.lo("Good Job Your Almost There!");
+// Console.log("If this appears in the console then you have completed the challenge, Well Done!")
+
+
+alert("Let's Begin The Challenge!");
+document.write("Congrats You Fixed This Line Of Code!");
+Console.log("Good Job Your Almost There!");
+Console.log("If this appears in the console then you have completed the challenge, Well Done!")
+
