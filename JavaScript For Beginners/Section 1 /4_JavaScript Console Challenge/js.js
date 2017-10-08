dc0364dc0e8ecb0a496c645f2158df0a4af257c4
dccruz <dccoder@icloud.com>
2017-10-07 20:42:09 -0700

    What is a Variable?

--- JavaScript For Beginners/Section 1 /4_JavaScript Console Challenge/js.js
+++ JavaScript For Beginners/Section 1 /4_JavaScript Console Challenge/js.js
@@ -11,5 +11,5 @@
 alert("Let's Begin The Challenge!");
 document.write("Congrats You Fixed This Line Of Code!");
 Console.log("Good Job Your Almost There!");
-Console.log("If this appears in the console then you have completed the challenge, Well Done!")
+Console.log("If this appears in the console then you have completed the challenge, Well Done!");
 
