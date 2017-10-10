210f43a50d5d4f55a261416cb5150d38fd6dc926
dccruz <dccoder@icloud.com>
2017-10-09 21:21:24 -0700

    Challenge 2

---
+++ JavaScript For Beginners/Section 2/4_Working With Variables and Strings Challenge/js.js
@@ -0,0 +1,8 @@
+
+alert("Welcome to DCC.com");
+var VisitorName = prompt("What is your name?");
+var VisitorFavColor = prompt("What is your favorite Color?");
+document.write("Hello "  +VisitorName+", your favorite color is " +VisitorFavColor+".");
+
+console.log("Program Complete");
+
