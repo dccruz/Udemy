632c3825d22d4dcaf4bb4609eb4408ee699fee16
dccruz <dccoder@icloud.com>
2017-10-10 21:41:33 -0700

    Days Alive
    Learning math in javascript

---
+++ JavaScript For Beginners/Section 3 /1_Days Alive/js.js
@@ -0,0 +1,7 @@
+
+
+
+var VisitorAge = prompt ('How many years have you been alive for?');
+var YearsAlive = parseInt(VisitorAge);
+YearsAlive = YearsAlive * 365; 
+document.write ('You have been alive for ' +YearsAlive+ ' Days!!!');
